7cc68e514a95b69622cd8bbbc1e76e2687b7198e
thea-aithre <theia.aithre@gmail.com>
2019-01-17 17:59:19 +0100

    Selenium & class name change

--- src/test/java/com/ninegag/test/features/MenuBarTest.java
+++ src/test/java/com/ninegag/test/features/MenuBarTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class UpvoteFeatureTest extends Selenium9gag {
+public class MenuBarTest extends Selenium9gag {
 
 
     @BeforeEach
